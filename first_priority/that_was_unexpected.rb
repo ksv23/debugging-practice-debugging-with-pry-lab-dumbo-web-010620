1bfe3038e3c150291add0497110962849f9333a7
Kring Villanueva <ksvillanueva7@gmail.com>
2020-01-03 06:59:13 +0000

    Automatically backed up by Learn

--- first_priority/that_was_unexpected.rb
+++ first_priority/that_was_unexpected.rb
@@ -1,35 +1,29 @@
-require 'pry'
-
 class Person
   attr_accessor :name, :candy_lover
 
   def initialize(name, candy_lover)
-
-
     @name = name
     @candy_lover = candy_lover
   end
 
-  def tall
+  def tall?
     true
   end
 
-  def basketball_player
+  def basketball_player?
     false
   end
 
-  def likes_candy
+  def likes_candy?
     true
   end
 
-  def ballin_candy_lover
-    binding.pry
-
-
+  def ballin_candy_lover?
     if @candy_lover == true
       "Aw sweet, this cat loves them some candy!"
     else
-        10.times do
-          "not a ballin candy lover"
-        end
+      10.times do
+        "not a ballin candy lover"
     end
+  end
+end
